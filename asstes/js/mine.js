f26b54113699caee0051c0e7328b1eebfd1a9145
hazrotali2006 <153986394+hazrotali2006@users.noreply.github.com>
2023-12-16 15:47:24 +0600

    i have a html file upload

---
+++ asstes/js/mine.js
@@ -0,0 +1,107 @@
+$(document).ready(function(){
+    
+    $(window).load(function() {
+   $("#loading").fadeOut(500);
+});
+
+// scrollTop
+$(window).load(function(){
+    $(window).on('scroll', function(){
+        if($(window).scrollTop() > 0){
+            $('.header-area').addClass('stick');
+        }
+        else{
+            $('.header-area').removeClass('stick');
+        }
+    })
+})
+$(window).load(function(){
+    $(window).on('scroll', function(){
+        if($(window).scrollTop() > 0){
+            $('.header-area2').addClass('stick');
+        }
+        else{
+            $('.header-area2').removeClass('stick');
+        }
+    })
+})
+// scrollUp js
+ $.scrollUp({
+    scrollName: 'scrollUp', // Element ID
+    topDistance: '300', // Distance from top before showing element (px)
+    topSpeed: 300, // Speed back to top (ms)
+    animation: 'fade', // Fade, slide, none
+    animationInSpeed: 200, // Animation in speed (ms)
+    animationOutSpeed: 200, // Animation out speed (ms)
+    scrollText: '<i class="fa-solid fa-arrow-up fa-fw"></i>', // Text for element
+    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+  });
+
+
+
+var animation_start_pos = 0, animation_end_pos = 1000; //where you want the animation to stop
+$(document).scroll(function() {
+var scroll_pos = $(this).scrollTop();
+var percentScrolled = parseFloat(scroll_pos/animation_end_pos);
+jQuery('.people-opacity').css('opacity', percentScrolled );
+});
+
+
+
+
+    
+
+$('#menu').slicknav();
+
+  // jQuery methods go here...
+$('.cass-stduies-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        768:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false,
+            loop:false
+        }
+    }
+});
+$('.singel-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:6,
+            nav:false,
+            loop:false
+        }
+    }
+});
+$('.testimonial-carousel').owlCarousel({
+    loop:true,
+    dots:true,
+    nav:false,
+    items:1,
+
+
+})
+
+});
